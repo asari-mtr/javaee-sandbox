8a5774190c3f2341d5c1a14b6a94a85d934db499
Asari Mitsuteru <asari.mtr@gmail.com>
2015-05-04 12:03:17 +0900

    アノテーションの追加

--- src/main/java/Startup.java
+++ src/main/java/Startup.java
@@ -1,5 +1,15 @@
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+
 /**
  * Created by asari on 2015/05/04.
  */
+@Singleton
+@javax.ejb.Startup
 public class Startup {
+    @PostConstruct
+    public void init() {
+        System.out.println("Hello Startup !!!!!");
+    }
 }
