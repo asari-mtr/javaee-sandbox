7071fb94481788b9890e857a3d1dc6046a817ec9
Asari Mitsuteru <asari.mtr@gmail.com>
2015-05-28 01:07:03 +0900

    viewParamの動きのテスト用準備

---
+++ src/main/java/action/Sample1Action.java
@@ -0,0 +1,7 @@
+package action;
+
+/**
+ * Created by asari on 2015/05/28.
+ */
+public class Sample1Action {
+}
---
+++ src/main/java/action/Sample2Action.java
@@ -0,0 +1,7 @@
+package action;
+
+/**
+ * Created by asari on 2015/05/28.
+ */
+public class Sample2Action {
+}
