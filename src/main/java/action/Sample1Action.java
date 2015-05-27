f843ef71422420af421d52718a32d75539e2364b
Asari Mitsuteru <asari.mtr@gmail.com>
2015-05-28 01:08:14 +0900

    paramからbeanに設定できるのかを試す

--- src/main/java/action/Sample1Action.java
+++ src/main/java/action/Sample1Action.java
@@ -1,7 +1,23 @@
 package action;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
 /**
  * Created by asari on 2015/05/28.
  */
+@Named
+@RequestScoped
 public class Sample1Action {
+    private int id;
+
+    public int getId() {
+        System.out.println(this + "sample1Action.getId " + id);
+        return id;
+    }
+
+    public void setId(int id) {
+        System.out.println(this + "sample1Action.setId " + id);
+        this.id = id;
+    }
 }
--- src/main/java/action/Sample2Action.java
+++ src/main/java/action/Sample2Action.java
@@ -1,7 +1,23 @@
 package action;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
 /**
  * Created by asari on 2015/05/28.
  */
+@Named
+@RequestScoped
 public class Sample2Action {
+    private int id;
+
+    public int getId() {
+        System.out.println(this + "sample2Action.getId " + id);
+        return id;
+    }
+
+    public void setId(int id) {
+        System.out.println(this + "sample2Action.setId " + id);
+        this.id = id;
+    }
 }
